373f5a1c6b76d34eeb19a170bdb52ac1d5b5f089
David Quevedo <dquevedor@gmail.com>
2013-08-14 16:24:48 -0500

    Vista requerimiento 1%

--- protected/views/requerimiento/view.php
+++ protected/views/requerimiento/view.php
@@ -3,17 +3,10 @@ $this->breadcrumbs=array(
 	'Requerimientos'=>array('index'),
 	'Requerimiento N° '.$model->IDREQUERIMIENTO,
 );
-
-$this->menu=array(
-	array('label'=>'List Requerimiento','url'=>array('index')),
-	array('label'=>'Create Requerimiento','url'=>array('create')),
-	array('label'=>'Update Requerimiento','url'=>array('update','id'=>$model->IDREQUERIMIENTO)),
-	array('label'=>'Delete Requerimiento','url'=>'#','linkOptions'=>array('submit'=>array('delete','id'=>$model->IDREQUERIMIENTO),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Requerimiento','url'=>array('admin')),
-);
 ?>
 
-<h1>View Requerimiento #<?php echo $model->IDREQUERIMIENTO; ?></h1>
+<h2 class ="center">Hoja de Requerimiento</h2>
+<h3 class="center">N° <?php echo $model->IDREQUERIMIENTO; ?></h3>
 
 <?php $this->widget('bootstrap.widgets.TbDetailView',array(
 	'data'=>$model,
