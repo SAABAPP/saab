ef48bb1ab776777e1be8b52846857d064ad7512e
Juan Carlos <carlosway89@gmail.com>
2013-09-17 01:18:33 -0500

    arreglo

--- protected/views/personal/index.php
+++ protected/views/personal/index.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 	header('Location:'.Yii::app()->request->baseUrl.'/personal/admin');
-=======
-=======
->>>>>>> origin/saabCesar
-/* @var $this BienController */
-/* @var $dataProvider CActiveDataProvider */
-	header('Location:'.Yii::app()->request->baseUrl.'/personal/admin.html');
->>>>>>> origin/saabCesar
+
 ?>
