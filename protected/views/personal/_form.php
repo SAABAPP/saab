38c83247c39cfe073a233ca7926daa1b85e71b2b
Carlosway89 <carlosway89@gmail.com>
2015-03-13 21:52:25 +0100

    new personal validation from

--- protected/views/personal/_form.php
+++ protected/views/personal/_form.php
@@ -8,15 +8,15 @@
 
 	<div class="control-group">
 		<?php echo $form->hiddenField($model,'PER_estado',array('class'=>'span3','value'=>'1')); ?>
-		<?php echo $form->textFieldRow($model,'PER_nombres',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Nombres..')); ?>				
-		<?php echo $form->textFieldRow($model,'PER_paterno',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Apellido Paterno..')); ?>				
-		<?php echo $form->textFieldRow($model,'PER_materno',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Apellido Materno..')); ?>
-		<?php echo $form->textFieldRow($model,'PER_dni',array('class'=>'span3','maxlength'=>8,'placeholder'=>'DNI..')); ?>
+		<?php echo $form->textFieldRow($model,'PER_nombres',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Nombres..','required'=>'required')); ?>				
+		<?php echo $form->textFieldRow($model,'PER_paterno',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Apellido Paterno..','required'=>'required')); ?>				
+		<?php echo $form->textFieldRow($model,'PER_materno',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Apellido Materno..','required'=>'required')); ?>
+		<?php echo $form->textFieldRow($model,'PER_dni',array('class'=>'span3','maxlength'=>8,'placeholder'=>'DNI..','required'=>'required')); ?>
 		<?php echo $form->radioButtonListRow($model, 'PER_sexo', array('H'=>'Hombre&nbsp;&nbsp;&nbsp;&nbsp;','M'=>'Mujer'),array('separator'=>'&nbsp;&nbsp;&nbsp;','labelOptions'=>array('style'=>'display:inline'))); ?>		
 		<?php echo $form->textFieldRow($model,'PER_direccion',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Direccion..')); ?>
 		<?php echo $form->textFieldRow($model,'PER_telefono',array('class'=>'span3','maxlength'=>12,'placeholder'=>'Telefono Fijo..')); ?>
 		<?php echo $form->textFieldRow($model,'PER_celular',array('class'=>'span3','maxlength'=>12,'placeholder'=>'Celular..')); ?>
-		<?php echo $form->textFieldRow($model,'PER_cargo',array('class'=>'span3','maxlength'=>60,'placeholder'=>'Cargo..')); ?>
+		<?php echo $form->textFieldRow($model,'PER_cargo',array('class'=>'span3','maxlength'=>60,'placeholder'=>'Cargo..','required'=>'required')); ?>
 		<?php echo $form->hiddenField($model,'PER_idResponsable',array('class'=>'span3','value'=>'0')); ?>
 		<label id="control-label" for="area" class="control-label">Área*</label>
 		<div class="controls">
@@ -27,9 +27,9 @@
 				'value'=>$model->isNewRecord?$model->IDAREA:$model->iDAREA->ARE_nombre,
 				'source'=>$this->createUrl('personal/buscaArea'),
 				'options'=>array(
-					'minLength'=>'1',
+					'minLength'=>'1'					
 					),                                                            
-				'htmlOptions'=>array('class'=>'span3','placeholder'=>'Buscar Area...'),  
+				'htmlOptions'=>array('class'=>'span3','placeholder'=>'Buscar Area...','required'=>'required'),  
 				'options'=>array(
 					'showAnim'=>'fold',
 					'select' => 'js:function(event, ui){
@@ -41,8 +41,8 @@
 		<?php echo $form->hiddenField($model,'IDAREA',array('class'=>'span5')); ?>	
 		</div>
 		<h2 class="center">Usuario del Sistema</h2><br>
-		<?php echo $form->textFieldRow($usuario,'USU_usuario',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Usuario..')); ?>
-		<?php echo $form->passwordFieldRow($usuario,'USU_password',array('class'=>'span3','maxlength'=>150,'placeholder'=>'En Blanco para no cambiar..','value'=>'')); ?>
+		<?php echo $form->textFieldRow($usuario,'USU_usuario',array('class'=>'span3','maxlength'=>150,'placeholder'=>'Usuario..','required'=>'required')); ?>
+		<?php echo $form->passwordFieldRow($usuario,'USU_password',array('class'=>'span3','maxlength'=>150,'autocomplete'=>'off','placeholder'=>$model->isNewRecord?'Ingresar Contraseña':'En Blanco para no cambiar..','value'=>'')); ?>
 		<label id="control-label" for="tipo" class="control-label">Tipo Usuario*</label>
 		<div class="controls">
 			<?php 
@@ -52,9 +52,10 @@
 				'value'=>$asignacion->itemname,
 				'source'=>$this->createUrl('personal/buscaAutorizacion'),
 				'options'=>array(
-					'minLength'=>'1',
+					'minLength'=>'1'
+					
 					),                                                            
-				'htmlOptions'=>array('class'=>'span3','placeholder'=>'Asignar Tipo Usuario...'),  
+				'htmlOptions'=>array('class'=>'span3','placeholder'=>'Asignar Tipo Usuario...','required'=>'required'),  
 				'options'=>array(
 					'showAnim'=>'fold',
 					'select' => 'js:function(event, ui){
